6f7ae3148b2142fe330084ecc6810fc01c643d28
rukshinia <rukshinia69@gmail.com>
2025-12-16 01:48:11 -0800

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ char c[5][10]={"    *","   ***","  *****"," *******","*********"};
+ int i;
+ for(i=0;i<5;i++)
+ {
+     printf("%s\n",c[i]);
+
+ }
+}
